7656646442f4ca3dbc1b4b229cc1a1924add0944
RacooLabs <window340@gmail.com>
2019-12-28 09:26:20 +0900

    commit

--- app/src/main/java/com/racoo/firststockcalculator/MainActivity.java
+++ app/src/main/java/com/racoo/firststockcalculator/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //이식 성공
 
         Intent intent = new Intent(this, LoadingActivity.class);
         startActivity(intent);
